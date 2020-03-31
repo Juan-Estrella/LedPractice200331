75dde2cb7ef49e4037f9885388ebc3eb669c225d
Juan-Estrella <brazenhouse@gmail.com>
2020-03-31 19:44:56 -0400

    DotStars Set Up

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define NUM_LEDS    144 
+#define DATA_PIN    28
+#define CLOCK_PIN   27
+#define COLOR_ORDER BGR
+#define BRIGHTNESS  3
+#define FRAMES_PER_SECOND  120
+CRGB leds[NUM_LEDS];
+
+
+void setup() 
+{
+  delay(5000); // 3 second delay for recovery
+  FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  Serial.println("Start up Finished");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  
 }
