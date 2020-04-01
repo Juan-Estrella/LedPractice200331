7a9f93a761c7f3130e1a917f9f1055566ee186f6
Juan-Estrella <brazenhouse@gmail.com>
2020-03-31 20:05:14 -0400

    Chasing LED in continual loop implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,28 @@
 #include <FastLED.h>
 
 #define NUM_LEDS    144 
-#define DATA_PIN    28
-#define CLOCK_PIN   27
+#define DATA_PIN    34
+#define CLOCK_PIN   33
 #define COLOR_ORDER BGR
 #define BRIGHTNESS  3
 #define FRAMES_PER_SECOND  120
 CRGB leds[NUM_LEDS];
 
+/************************************
+Purpose:
+Gradually Fades LEDs after they turn on
+
+Parameters: 
+NA
+The lower the .nscale argument, the faster the LEDs turn off (i.e. shorter the trail is.
+*****/
+void fadeall() 
+{ 
+  for(int i = 0; i < NUM_LEDS; i++) 
+    { 
+    leds[i].nscale8(120); 
+    } 
+}
 
 void setup() 
 {
@@ -20,5 +35,11 @@ void setup()
 
 void loop() 
 {
-  
+   for (int x = 0; x <144; x++) //moves LEDs forward
+    {
+      leds[x] = CHSV(255, 255, 255);
+      FastLED.show();
+      fadeall();
+      delay(25);
+    } 
 }
