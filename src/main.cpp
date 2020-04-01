c5be7ad89c0faa2ce37d7fbe77c4158107ff5b0e
Juan-Estrella <brazenhouse@gmail.com>
2020-03-31 20:42:02 -0400

    Keyboard 'a' to trigger.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS    144 
-#define DATA_PIN    34
-#define CLOCK_PIN   33
+#define NUM_LEDS 144
+#define DATA_PIN 34
+#define CLOCK_PIN 33
 #define COLOR_ORDER BGR
-#define BRIGHTNESS  3
-#define FRAMES_PER_SECOND  120
+#define BRIGHTNESS 3
+#define FRAMES_PER_SECOND 120
 CRGB leds[NUM_LEDS];
 
 /**************Function***************
@@ -17,15 +17,15 @@ Parameters:
 NA
 The lower the .nscale argument, the faster the LEDs turn off (i.e. shorter the trail is.
 *************************************/
-void fadeall() 
-{ 
-  for(int i = 0; i < NUM_LEDS; i++) 
-    { 
-    leds[i].nscale8(120); 
-    } 
+void fadeall()
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    leds[i].nscale8(120);
+  }
 }
 //**************Set Up****************
-void setup() 
+void setup()
 {
   delay(5000); // 3 second delay for recovery
   FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
@@ -33,15 +33,21 @@ void setup()
   Serial.println("Start up Finished");
 }
 //*************Loop*******************
-void loop() 
+void loop()
 {
-   for (int x = 0; x <144; x++) //moves LEDs forward
-    {
-      leds[x] = CHSV(255, 255, 255);
-      FastLED.show();
-      fadeall();
-      delay(25);
-      int asdfsdfs;
-    } 
+  if (Serial.available())
+  {
+    char ch = Serial.read();
 
+    if (ch == 'a')
+    {
+      for (int x = 0; x < 144; x++) //moves LEDs forward
+      {
+        leds[x] = CHSV(255, 255, 255);
+        FastLED.show();
+        fadeall();
+        delay(25);
+      }
+    }
+  }
 }
