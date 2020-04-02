58c463f28ba9f37dde9acae3094f43e0deab6c2c
Juan-Estrella <theenlightenedjuan@gmail.com>
2020-04-01 22:36:48 -0400

    Provides ability to snd multiple pulses down strip

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
 #include <FastLED.h>
-
+//***********Constants*****************
 #define NUM_LEDS 144
 #define DATA_PIN 34
 #define CLOCK_PIN 33
 #define COLOR_ORDER BGR
 #define BRIGHTNESS 3
 #define FRAMES_PER_SECOND 120
-CRGB leds[NUM_LEDS];
+const long period = 100;                       //delay between increments
 
+//*************Variables*************
+CRGB leds[NUM_LEDS];
+long startMillis = 0;
+int x = 0;
 /**************Function***************
 Purpose:
-Gradually Fades LEDs after they turn on
-
+Provides ability to send multiple led pulses down strip simultaneously.
+Based on "LedChasingFullySolved191015c"
 Parameters: 
 NA
-The lower the .nscale argument, the faster the LEDs turn off (i.e. shorter the trail is.
 *************************************/
-void fadeall()
+void NextPulse()
 {
-  for (int i = 0; i < NUM_LEDS; i++)
+  for (int c = NUM_LEDS - 1; c >= 1; c--)
   {
-    leds[i].nscale8(120);
+    leds[c] = leds[c - 1];
   }
+  leds[0] = CRGB::Black;
+  FastLED.show();
 }
 //**************Set Up****************
 void setup()
@@ -35,19 +40,43 @@ void setup()
 //*************Loop*******************
 void loop()
 {
-  if (Serial.available())
+  unsigned long currentMillis = millis();      // Note the time
+  if (Serial.available() > 0)                  // Check for inputs from the serial line
   {
-    char ch = Serial.read();
-
-    if (ch == 'a')
+    byte inByte = Serial.read();               // Read the input character
+    switch (inByte)
     {
-      for (int x = 0; x < 144; x++) //moves LEDs forward
-      {
-        leds[x] = CHSV(255, 255, 255);
-        FastLED.show();
-        fadeall();
-        delay(25);
-      }
-    }
+      case 'a':                               // Start a new pulse train
+        Serial.println("a");                   // echo the input
+        leds[x] = CRGB::Blue;
+        break;
+      case 'b':
+        Serial.println("b");
+        leds[x+1] = CRGB::OrangeRed;
+        break;
+      case 'c':
+        Serial.println("c");
+        leds[x+2] = CRGB::Gold;
+        break;
+      case 'd':
+        Serial.println("d");
+        leds[x+3] = CRGB::DarkViolet;
+        break;
+       case 'e':
+        Serial.println("e");
+        leds[x+4] = CRGB::Red;
+        break;
+
+        case 'f':
+        Serial.println("f");
+        leds[x+5] = CRGB::Green;
+        break;
+      default:;
+    }//switch
+  }//if
+  if (currentMillis - startMillis >= period)
+  {
+    startMillis = currentMillis;                  // reset the start time for the next period
+    NextPulse();                                   //move led down the strip
   }
-}
+}//loop
